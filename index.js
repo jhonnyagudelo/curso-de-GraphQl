54e3f80984c7829028e6c327d8646ff199a1f471
jhonny stiven agudelo tenorio <jhonny.agudelo3120@gmail.com>
2021-11-06 12:28:12 -0500

    mutatuion

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-import { gql } from "apollo-server-core";
-import { ApolloServer } from "apollo-server";
+import { ApolloServer, UserInputError, gql } from "apollo-server";
+import { v1 as uuid } from "uuid";
 const persons = [
   {
+    age: "32",
     name: "jhonny",
-    phone: "312-747-0718",
     street: "calle 34a",
     city: "Palmira",
     cc: "11364502",
   },
   {
+    age: "13",
     name: "stiven",
     phone: "312-700-0718",
     street: "calle 15 107",
@@ -16,6 +17,7 @@ const persons = [
     cc: "113648956",
   },
   {
+    age: "16",
     name: "diven",
     phone: "312-747-002",
     street: "calle 58",
@@ -25,24 +27,95 @@ const persons = [
 ];
 
 const typeDefs = gql`
+  enum YesNo {
+    YES
+    NO
+  }
+  type Address {
+    street: String!
+    city: String!
+  }
   type Person {
     name: String!
     phone: String
-    street: String
-    city: String
-    cc: ID!
+    address: Address!
+    canDrink: String!
+    id: ID!
   }
 
   type Query {
     personCount: Int!
-    allPersons: [Person]!
+    allPersons(phone: YesNo): [Person]!
+    findPerson(name: String!): Person
+  }
+
+  type Mutation {
+    addPerson(
+      name: String!
+      phone: String
+      street: String!
+      city: String!
+      age: String
+    ): Person
+
+    editNumber(name: String!, phone: String!): Person
   }
 `;
 
 const resolvers = {
   Query: {
     personCount: () => persons.length,
-    allPersons: () => persons,
+    allPersons: (root, args) => {
+      if (!args.phone) return persons;
+
+      const byPhone = (person) =>
+        args.phone === "YES" ? person.phone : !person.phone;
+      return persons.filter(byPhone);
+    },
+    findPerson: (root, args) => {
+      const { name } = args;
+      return (
+        persons.find((person) => person.name === name) || { name: "Not found" }
+      );
+    },
+  },
+  // laura marcela valencia
+  Mutation: {
+    addPerson: (root, args) => {
+      //const { name,   phone, street, city } = args
+      if (persons.find((p) => p.name === args.name)) {
+        throw new UserInputError("Name must be unique", {
+          invalidArgs: args.name,
+        });
+      }
+      const person = { ...args, id: uuid() };
+      persons.push(person); //update database with new person
+      return person;
+    },
+
+    editNumber: (root, args) => {
+      const personIndex = persons.findIndex((p) => p.name === args.name);
+      if (!personIndex === -1) return null;
+      const person = persons[personIndex];
+
+      const updatedPerson = { ...person, phone: args.phone };
+      persons[personIndex] = updatedPerson;
+      return updatedPerson;
+    },
+  },
+
+  //otras formas
+  //sirve para crear calculos
+  Person: {
+    canDrink: (root) => root.age > 18,
+    address: (root) => `${root.street}, ${root.city}`,
+    //diferenciar la informacion de la db y como preguntar por la informacion y como recuoperar en el cliente
+    address: (root) => {
+      return {
+        street: root.street,
+        city: root.city,
+      };
+    },
   },
 };
 
